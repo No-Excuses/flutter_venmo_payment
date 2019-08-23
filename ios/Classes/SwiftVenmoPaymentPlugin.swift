87b8c379625fad3c1d7cc0d7458791101c4a454e
Gaurav Kale <kalegd@dukes.jmu.edu>
2019-08-23 01:01:45 -0400

    Remove passing back Transaction Object

--- ios/Classes/SwiftVenmoPaymentPlugin.swift
+++ ios/Classes/SwiftVenmoPaymentPlugin.swift
@@ -43,7 +43,6 @@ public class SwiftVenmoPaymentPlugin: NSObject, FlutterPlugin {
               responseParams["id"] = transaction?.transactionID ?? ""
               responseParams["error"] = (error)?.localizedDescription ?? ""
               responseParams["success"] = success
-              responseParams["transaction"] = transaction ?? "Sucks to suck"
               result (responseParams)
             })
       } else {
